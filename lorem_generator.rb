ed81230f0ba497249bd8234a95b55079e3b1ccbf
Melisa Budini <melixbq@gmail.com>
2020-08-22 12:02:44 -0400

    lorem generator agregado

---
+++ lorem_generator.rb
@@ -0,0 +1,6 @@
+n = ARGV[0].to_i
+
+n.times do 
+    puts "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque maximus lorem sit amet dolor lobortis, vitae efficitur odio blandit. Integer pellentesque fringilla turpis, ac sagittis nisi bibendum et. Cras a purus arcu. Quisque facilisis volutpat ipsum vitae ullamcorper. Vestibulum hendrerit, orci vitae lacinia semper, ex ligula efficitur augue, eu laoreet enim felis ut mi. Vivamus nisl ligula, faucibus quis rutrum in, pulvinar eget mi. Phasellus tristique libero sit amet blandit pharetra. Aliquam erat volutpat."
+    puts
+end
