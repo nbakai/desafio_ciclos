b1f62bad284b8527ea87af4d0b9033e55de73843
Melisa Budini <melixbq@gmail.com>
2020-08-22 17:31:42 -0400

    gen agregado

---
+++ gen.rb
@@ -0,0 +1,9 @@
+
+def gen(n)
+    arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    n.times do |i|
+     print arr[i]
+    end
+end
+gen(5)
+puts
